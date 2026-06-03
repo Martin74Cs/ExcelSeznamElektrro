a0becb2fcb2d903e3001ba1f7c130e695c95efa7
Martin <56979879+Martin74Cs@users.noreply.github.com>
2026-06-03 09:07:13 +0200

    Prace

--- WinForms/Form1.cs
+++ WinForms/Form1.cs
@@ -300,18 +300,18 @@ namespace WinForms
             var result = table.ShowDialog();
             if(result == DialogResult.OK) {
 
-                Data.SaveJsonList(Informace.Create.SouborStrojeJson);
+                Data.SaveJsonList(Informace.Instance.SouborStrojeJson);
 
                 //if (Data.Count < 1) Data.Add(new Zarizeni());
-                //Data.SaveJsonList(Informace.Create.SouborStrojeJson);
+                //Data.SaveJsonList(Informace.Instance.SouborStrojeJson);
 
                 // Zde můžete provést další akce po zavření dialogu
                 // Například načíst data nebo aktualizovat UI
             }
             else if(result == DialogResult.Cancel) {
                 Console.WriteLine($"DialogResult.Cancel");
-                Console.WriteLine($"Soubor : {Path.GetFileName(Informace.Create.SouborStrojeJson)} - ULOŽEN.");
-                Data.SaveJsonList(Informace.Create.SouborStrojeJson);
+                Console.WriteLine($"Soubor : {Path.GetFileName(Informace.Instance.SouborStrojeJson)} - ULOŽEN.");
+                Data.SaveJsonList(Informace.Instance.SouborStrojeJson);
             }
         }
 
